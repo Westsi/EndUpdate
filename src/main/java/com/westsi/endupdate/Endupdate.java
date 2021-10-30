bdc024986d32fb53de6949976204f5a1947eb069
Westsi <76999267+Westsi@users.noreply.github.com>
2021-10-30 13:01:19 +0100

    Add files via upload

---
+++ src/main/java/com/westsi/endupdate/Endupdate.java
@@ -0,0 +1,36 @@
+package com.westsi.endupdate;
+
+import com.westsi.endupdate.block.ModBlocks;
+import com.westsi.endupdate.item.ModItems;
+import com.westsi.endupdate.util.ModLootTableModifiers;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+
+import static com.westsi.endupdate.worldgen.OreGenEnd.ORE_WOOL_OVERWORLD;
+
+public class Endupdate implements ModInitializer {
+
+    public static final String MOD_ID = "endupdate";
+
+    @Override
+    public void onInitialize() {
+
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+        ModLootTableModifiers.modifyLootTables();
+
+
+        RegistryKey<ConfiguredFeature<?, ?>> oreWoolOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("tutorial", "ore_wool_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreWoolOverworld.getValue(), ORE_WOOL_OVERWORLD);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreWoolOverworld);
+
+    }
+}
